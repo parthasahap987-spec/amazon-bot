2b6c2b478bfe8902095cf4f7fb4466d203625f7a
parthasahap987-spec <parthasahap987@gmail.com>
2026-03-14 01:51:43 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,18 +1,46 @@
-import time
+import re
 import requests
+import time
 
 BOT_TOKEN = "8799971120:AAHlHlFBghuS73mBBaUI27PA1Ih45f1NhCw"
 CHANNEL_ID = "-1002161382456"
+AFFILIATE_TAG = "partha07e-21"
+
+def get_amazon_link(text):
+    links = re.findall(r'https?://\S+', text)
+    for link in links:
+        if "amazon." in link or "amzn.to" in link:
+            return link
+    return None
+
+def add_tag(link):
+    link = link.split("?")[0]
+    return f"{link}?tag={AFFILIATE_TAG}"
+
+def send(msg):
+    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+    data = {
+        "chat_id": CHANNEL_ID,
+        "text": msg,
+        "disable_web_page_preview": True
+    }
+    requests.post(url, data=data)
+
+print("Amazon bot started")
 
 while True:
 
-    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+    link = "https://www.amazon.in/dp/B0CXXXXXXX"
 
-    data = {
-        "chat_id": CHANNEL_ID,
-        "text": "✅ Bot working!"
-    }
+    affiliate = add_tag(link)
 
-    requests.post(url, data=data)
+    text = f"""
+🔥 Amazon Deal
 
-    time.sleep(60)
+🛒 Buy Now
+{affiliate}
+"""
+
+    send(text)
+
+    time.sleep(3600)
