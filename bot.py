c63341bd5621110fc4f3aef79ae58680a2b0bbfa
parthasahap987-spec <parthasahap987@gmail.com>
2026-03-14 01:49:15 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,11 +1,18 @@
 import time
-from telegram import Bot
+import requests
 
 BOT_TOKEN = "8799971120:AAHlHlFBghuS73mBBaUI27PA1Ih45f1NhCw"
-CHANNEL_ID = -1002161382456
-
-bot = Bot(token=BOT_TOKEN)
+CHANNEL_ID = "-1002161382456"
 
 while True:
-    bot.send_message(CHANNEL_ID, "✅ Bot working!")
+
+    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+
+    data = {
+        "chat_id": CHANNEL_ID,
+        "text": "✅ Bot working!"
+    }
+
+    requests.post(url, data=data)
+
     time.sleep(60)
